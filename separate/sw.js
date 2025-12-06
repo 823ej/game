28a555835ce6915aee756e93a64a6d438a564eeb
823ej <823kej@gmail.com>
2025-12-07 00:52:20 +0900

    Add PWA support for home screen install

---
+++ separate/sw.js
@@ -0,0 +1,34 @@
+// Simple passthrough service worker for PWA installability.
+const CACHE_NAME = 'midnight-shell-v1';
+const SHELL_FILES = [
+  './',
+  './index.html',
+  './style.css',
+  './game.js',
+  './data.js',
+  './story.js',
+  './manifest.webmanifest'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(SHELL_FILES)).catch(() => {})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    ).catch(() => {})
+  );
+  clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  const { request } = event;
+  event.respondWith(
+    caches.match(request).then(cached => cached || fetch(request))
+  );
+});
